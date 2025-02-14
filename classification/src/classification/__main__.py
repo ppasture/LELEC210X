a3b77c2311d6823bc5b47fac7d4535a617feca29
Hubert Badoux <hubert.badoux@student.uclouvain.be>
2025-02-14 13:29:59 +0100

    classify

--- classification/src/classification/__main__.py
+++ classification/src/classification/__main__.py
@@ -1,69 +1,169 @@
+import os
 import pickle
 from pathlib import Path
+from collections.abc import Iterator
 from typing import Optional
 
 import click
-
+import serial
+import zmq
+import numpy as np
+import requests
+import json
 import common
 from auth import PRINT_PREFIX
 from common.env import load_dotenv
 from common.logging import logger
-
+hostname = "http://localhost:5000"  
+key = "jNvyuAfUUwf3iZAWF40sqSuW3DHRkjTj8jwDb0-d"
 from .utils import payload_to_melvecs
+import sys
+import os
 
+sys.path.append(os.path.abspath("auth/src"))
+
+from auth import packet
 load_dotenv()
 
+def parse_packet(line: str) -> Optional[bytes]:
+    """Parse a line into a packet."""
+    line = line.strip()
+    if line.startswith(PRINT_PREFIX):
+        return bytes.fromhex(line[len(PRINT_PREFIX) :])
+    return None
+
+def hex_to_bytes(ctx: click.Context, param: click.Parameter, value: str) -> bytes:
+    """Convert a hex string into bytes."""
+    return bytes.fromhex(value)
 
 @click.command()
 @click.option(
     "-i",
     "--input",
     "_input",
-    default="-",
+    default=None,
     type=click.File("r"),
-    help="Where to read the input stream. Default to '-', a.k.a. stdin.",
+    help="Where to read the input stream. If not specified, read from TCP address. You can pass '-' to read from stdin.",
 )
 @click.option(
-    "-m",
-    "--model",
+    "-o",
+    "--output",
+    default="-",
+    type=click.File("w"),
+    help="Where to write the output stream. Default to '-', a.k.a. stdout.",
+)
+@click.option(
+    "--serial-port",
     default=None,
-    type=click.Path(exists=True, dir_okay=False, path_type=Path),
-    help="Path to the trained classification model.",
+    envvar="SERIAL_PORT",
+    show_envvar=True,
+    help="If specified, read the packet from the given serial port. E.g., '/dev/tty0'. This takes precedence over `--input` and `--tcp-address` options.",
+)
+@click.option(
+    "--tcp-address",
+    default="tcp://127.0.0.1:10000",
+    envvar="TCP_ADDRESS",
+    show_default=True,
+    show_envvar=True,
+    help="TCP address to be used to read the input stream.",
+)
+@click.option(
+    "-k",
+    "--auth-key",
+    default=16 * "00",
+    envvar="AUTH_KEY",
+    callback=hex_to_bytes,
+    show_default=True,
+    show_envvar=True,
+    help="Authentication key (hex string).",
+)
+@click.option(
+    "--authenticate/--no-authenticate",
+    default=True,
+    is_flag=True,
+    help="Enable / disable authentication, useful for skipping authentication step.",
 )
 @common.click.melvec_length
 @common.click.n_melvecs
 @common.click.verbosity
 def main(
     _input: Optional[click.File],
-    model: Optional[Path],
+    output: click.File,
+    serial_port: Optional[str],
+    tcp_address: str,
+    auth_key: bytes,
+    authenticate: bool,
     melvec_length: int,
     n_melvecs: int,
 ) -> None:
     """
-    Extract Mel vectors from payloads and perform classification on them.
-    Classify MELVECs contained in payloads (from packets).
-
-    Most likely, you want to pipe this script after running authentification
-    on the packets:
-
-        rye run auth | rye run classify
-
-    This way, you will directly receive the authentified packets from STDIN
-    (standard input, i.e., the terminal).
+    Parse packets from the MCU, perform authentication, and classify MELVECs contained in payloads.
     """
-    if model:
-        with open(model, "rb") as file:
-            m = pickle.load(file)
-    else:
-        m = None
+    logger.debug(f"Unwrapping packets with auth. key: {auth_key.hex()}")
+    
+    unwrapper = packet.PacketUnwrapper(
+        key=auth_key,
+        allowed_senders=[0],
+        authenticate=authenticate,
+    )
+    
+    def reader() -> Iterator[str]:
+        if serial_port:
+            ser = serial.Serial(port=serial_port, baudrate=115200)
+            ser.reset_input_buffer()
+            ser.read_until(b"\n")
+            logger.debug(f"Reading packets from serial port: {serial_port}")
+            while True:
+                line = ser.read_until(b"\n").decode("ascii").strip()
+                packet_data = parse_packet(line)
+                if packet_data:
+                    yield packet_data
+        elif _input:
+            logger.debug(f"Reading packets from input: {_input!s}")
+            for line in _input:
+                packet_data = parse_packet(line)
+                if packet_data:
+                    yield packet_data
+        else:
+            context = zmq.Context()
+            socket = context.socket(zmq.SUB)
+            socket.setsockopt(zmq.SUBSCRIBE, b"")
+            socket.setsockopt(zmq.CONFLATE, 1)
+            socket.connect(tcp_address)
+            logger.debug(f"Reading packets from TCP address: {tcp_address}")
+            while True:
+                yield socket.recv(2 * melvec_length * n_melvecs)
+    
+    input_stream = reader()
+    with open('classification/data/models/final_model.pickle', 'rb') as f:
+        m = pickle.load(f)
 
-    for payload in _input:
-        if PRINT_PREFIX in payload:
-            payload = payload[len(PRINT_PREFIX) :]
+    for msg in input_stream:
+        try:
+            sender, payload = unwrapper.unwrap_packet(msg)
+            logger.debug(f"From {sender}, received packet: {payload.hex()}")
+            print("Received packet:")
+            print(payload.hex())
 
-            melvecs = payload_to_melvecs(payload, melvec_length, n_melvecs)
+            output.write(PRINT_PREFIX + payload.hex() + "\n")
+            output.flush()
+            melvecs = payload_to_melvecs(payload.hex(), melvec_length, n_melvecs)
             logger.info(f"Parsed payload into Mel vectors: {melvecs}")
+            melvec = melvecs/np.linalg.norm(melvecs)
+            melvec = melvec.reshape(1, -1)
+            proba_knn = m.predict_proba(melvec)
+            prediction = m.predict(melvec)
+            
+            logger.info(f"Predictions: {proba_knn}")
+            logger.info(f"Prediction: {prediction}")
+            
+            answer = requests.post(f"{hostname}/lelec210x/leaderboard/submit/{key}/{prediction}", timeout=1)
+            
+            json_answer = json.loads(answer.text)
+            print(json_answer)
 
-            if m:
-                # TODO: perform classification
-                pass
+        
+        except packet.InvalidPacket as e:
+            logger.error(f"Invalid packet error: {e.args[0]}")
+        except Exception as e:
+            logger.exception(f"Unexpected error while processing packet: {e}")
