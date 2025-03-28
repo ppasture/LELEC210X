82809e3903018828023614d9200e4e918d35c377
Hubert Badoux <hubert.badoux@student.uclouvain.be>
2025-03-28 15:34:13 +0100

    audio acquisition

--- mcu/hands_on_audio_acquisition/uart-reader.py
+++ mcu/hands_on_audio_acquisition/uart-reader.py
@@ -1,53 +1,34 @@
-"""
-uart-reader.py
-ELEC PROJECT - 210x
-"""
-
 import argparse
-
 import matplotlib.pyplot as plt
 import numpy as np
 import serial
 import soundfile as sf
 from serial.tools import list_ports
 
-PRINT_PREFIX = "SND:HEX:"
 FREQ_SAMPLING = 10200
 VAL_MAX_ADC = 4096
 VDD = 3.3
-
-
-def parse_buffer(line):
-    line = line.strip()
-    if line.startswith(PRINT_PREFIX):
-        return bytes.fromhex(line[len(PRINT_PREFIX) :])
-    else:
-        print(line)
-        return None
-
+LABELS = ["garbage"]
+SAMPLES_PER_LABEL = 40
+BUFFER_SIZE = 50000  # 25000 échantillons * 2 octets
 
 def reader(port=None):
     ser = serial.Serial(port=port, baudrate=115200)
     while True:
-        line = ""
-        while not line.endswith("\n"):
-            line += ser.read_until(b"\n", size=1042).decode("ascii")
-        line = line.strip()
-        buffer = parse_buffer(line)
-        if buffer is not None:
-            dt = np.dtype(np.uint16)
-            dt = dt.newbyteorder("<")
-            buffer_array = np.frombuffer(buffer, dtype=dt)
-
-            yield buffer_array
-
+        buffer = ser.read(BUFFER_SIZE)
+        if len(buffer) != BUFFER_SIZE:
+            print("Erreur: paquet incomplet")
+            continue
+        dt = np.dtype(np.uint16)
+        dt = dt.newbyteorder("<")  # Little endian
+        buffer_array = np.frombuffer(buffer, dtype=dt)
+        yield buffer_array
 
 def generate_audio(buf, file_name):
     buf = np.asarray(buf, dtype=np.float64)
     buf = buf - np.mean(buf)
     buf /= max(abs(buf))
-    sf.write("audio_files/" + file_name + ".ogg", buf, FREQ_SAMPLING)
-
+    sf.write(f"{file_name}.wav", buf, FREQ_SAMPLING)
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
@@ -56,37 +37,33 @@ if __name__ == "__main__":
     print("uart-reader launched...\n")
 
     if args.port is None:
-        print(
-            "No port specified, here is a list of serial communication port available"
-        )
+        print("No port specified, here is a list of serial communication port available")
         print("================")
         port = list(list_ports.comports())
         for p in port:
             print(p.device)
         print("================")
         print("Launch this script with [-p PORT_REF] to access the communication port")
-
     else:
         plt.figure(figsize=(10, 5))
         input_stream = reader(port=args.port)
         msg_counter = 0
+        label_index = 0
 
         for msg in input_stream:
-            print(f"Acquisition #{msg_counter}")
+            if label_index >= len(LABELS):
+                print("Acquisition complete.")
+                break
+
+            file_name = f"{LABELS[label_index]}_{msg_counter % SAMPLES_PER_LABEL:02d}"
+            print(f"Acquisition #{msg_counter}: Saving as {file_name}.wav")
 
             buffer_size = len(msg)
             times = np.linspace(0, buffer_size - 1, buffer_size) * 1 / FREQ_SAMPLING
             voltage_mV = msg * VDD / VAL_MAX_ADC * 1e3
 
-            plt.plot(times, voltage_mV)
-            plt.title(f"Acquisition #{msg_counter}")
-            plt.xlabel("Time (s)")
-            plt.ylabel("Voltage (mV)")
-            plt.ylim([0, 3300])
-            plt.draw()
-            plt.pause(0.001)
-            plt.cla()
-
-            generate_audio(msg, f"acq-{msg_counter}")
-
+            generate_audio(msg, file_name)
             msg_counter += 1
+
+            if msg_counter % SAMPLES_PER_LABEL == 0:
+                label_index += 1
